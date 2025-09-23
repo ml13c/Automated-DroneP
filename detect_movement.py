f3967a8a0a96ced7b97d98a8398aea872792e017
ml13c <marcuslara240@gmail.com>
2025-09-23 18:00:37 -0500

    added old files

--- detect_movement.py
+++ detect_movement.py
@@ -12,7 +12,7 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mp_draw = mp.solutions.drawing_utils
 
-# Webcam
+# webcam(desktop)/ pi ai camera
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # width
 cap.set(4, 480)  # height
